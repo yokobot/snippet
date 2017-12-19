d4f3fb8ae2450ed7588f7d5be175f6cd549d9589
yokobot <takuou@gmail.com>
2017-12-19 19:21:44 +0900

    テストの準備

---
+++ aws/lambda/test_console_login_check.py
@@ -0,0 +1,18 @@
+"""
+console_login_check のテストコード
+"""
+# coding: utf-8
+
+import unittest
+import boto3
+from mock import Mock
+from console_login_check import ConsoleLoginCheck
+
+class TestConsoleLoginCheck(unittest.TestCase):
+    """
+    テスト用クラス
+    """
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
