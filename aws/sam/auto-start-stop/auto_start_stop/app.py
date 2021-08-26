6420f88c5b1bf33071161545c93f8adbbc1db122
takao.yokoyama <takao.yokoyama@miidas.jp>
2021-08-26 19:21:46 +0900

    ecsを自動起動停止に追加

--- aws/sam/auto-start-stop/auto_start_stop/app.py
+++ aws/sam/auto-start-stop/auto_start_stop/app.py
@@ -13,6 +13,7 @@ logger.setLevel(logging.INFO)
 
 
 ec2 = boto3.client('ec2')
+ecs = boto3.client('ecs')
 rds = boto3.client('rds')
 sts = boto3.client('sts')
 AWS_ACCOUNT = sts.get_caller_identity()['Account']
@@ -84,6 +85,73 @@ def start_stop_ec2_instance(ec2_target):
             Force=True
         )
     logger.info("start_stop_ec2_instance is end.")
+
+
+def get_ecs_target_list():
+    """
+    任意のタグを持つ ECS サービスの cluster_arn と service_arn を配列で返す
+    """
+    logger.info("get_ec2_target_list is start.")
+    response = ecs.list_clusters()
+    ecs_target_list = []
+    for cluster_arn in response['clusterArns']:
+        response = ecs.list_services(
+            cluster=cluster_arn
+        )
+        for service_arn in response['serviceArns']:
+            response = ecs.describe_services(
+                cluster=cluster_arn,
+                services=[
+                    service_arn,
+                ],
+                include=[
+                    'TAGS',
+                ]
+            )
+            if response['services'][0].get('tags'):
+                for tag in response['services'][0]['tags']:
+                    if tag['key'] == 'AutoStartStop' and tag['value'] == 'true':
+                        ecs_target_list.append({'cluster': cluster_arn, 'service': response['services'][0]['serviceArn']})
+    logger.info("get_ec2_target_list is end.")
+    return ecs_target_list
+
+
+def start_stop_ecs_service(ecs_target):
+    """
+    タグ StartTime, StopTime で指定された時間にタスク数を変更する
+    """
+    logger.info("start_stop_ecs_service is start.")
+    jst = timezone(timedelta(hours=9), 'JST')
+    now = datetime.now(jst)
+    logger.info("Exec time is %s .", now.hour)
+    response = ecs.list_tags_for_resource(
+        resourceArn=ecs_target['service']
+    )
+    for tag in response['tags']:
+        if tag['key'] == 'AutoStartStop' and tag['value'] == 'true':
+            logger.info("%s is target service.", ecs_target['service'])
+            for start_tag in response['tags']:
+                if start_tag['key'] == 'StartTime':
+                    if str(now.hour) == start_tag['value']:
+                        if 0 <= int(now.weekday()) <= 4:
+                            response = ecs.update_service(
+                                cluster=ecs_target['cluster'],
+                                service=ecs_target['service'],
+                                desiredCount=1
+                            )
+                            logger.info(response)
+                elif start_tag['key'] == 'StopTime':
+                    if str(now.hour) == start_tag['value']:
+                        response = ecs.update_service(
+                            cluster=ecs_target['cluster'],
+                            service=ecs_target['service'],
+                            desiredCount=0
+                        )
+                        logger.info(response)
+            break
+    else:
+        logger.info("%s is not target instance.", ecs_target['service'])
+    logger.info("start_stop_ecs_service is end.")
 
 
 def get_rds_target_list():
@@ -210,6 +278,8 @@ def lambda_handler(event, context):
     logger.info("lambda function is start.")
     for ec2_target in get_ec2_target_list():
         start_stop_ec2_instance(ec2_target)
+    for ecs_target in get_ecs_target_list():
+        start_stop_ecs_service(ecs_target)
     for rds_target in get_rds_target_list():
         start_stop_rds_instance(rds_target, 'RDS')
     for aurora_target in get_aurora_target_list():
